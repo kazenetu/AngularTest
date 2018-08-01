7e09ca0312a877f6541d6a51115484c0974001fc
kazenetu <rakudafactory@gmail.com>
2018-08-02 00:10:48 +0900

    ルーティングのパスミスを修正

--- practice/src/app/app.module.ts
+++ practice/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
